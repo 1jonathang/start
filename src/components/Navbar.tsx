8332c457893c77e2c6055248f0b6b6315e8e8c7c
jonathangorbachev <jonathangorbachev@gmail.com>
2023-06-23 15:30:04 -0400

    final navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,11 +62,14 @@ const Navbar: React.FC<NavbarProps> = ({ session }) => {
             onClose={setIsOpen}
           >
             <div className="fixed inset-0 z-50" />
-            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto dark:bg-slate-800 bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="font-bold">BookStore</span>
-                </a>
+                <Link
+                  href={"/"}
+                  className="ml-3 text-slate-900 dark:text-slate-400 font-semibold text-lg"
+                >
+                  COMPANY
+                </Link>
                 <button
                   type="button"
                   className="-m-2.5 rounded-md p-2.5 text-gray-700"
